d312d82fd13729c535900bc3ba3c524deca0943e
rohanjariwala03 <34390549+rohanjariwala03@users.noreply.github.com>
2017-12-25 14:17:27 +0530

    Add files via upload

---
+++ LapTimeManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimeManager : MonoBehaviour {
+
+	public static int MinuteCount;
+	public static int SecondCount;
+	public static float MilliCount;
+	public static string MilliDisplay;
+
+	public GameObject Minutebox;
+	public GameObject Secondbox;
+	public GameObject Millibox;
+
+	public static float RawTime;
+
+	void Update () 
+	{
+		MilliCount += Time.deltaTime * 10;
+		RawTime += Time.deltaTime;
+		MilliDisplay = MilliCount.ToString ("F0");
+		Millibox.GetComponent<Text> ().text = "" + MilliDisplay;
+
+		if (MilliCount >= 10)
+		{
+			MilliCount = 0;
+			SecondCount += 1;
+		}
+
+		if (SecondCount <= 9) {
+			Secondbox.GetComponent<Text> ().text = "0" + SecondCount + ".";
+		}
+		else
+		{
+			Secondbox.GetComponent<Text>().text= "" +SecondCount+".";		
+		}
+
+		if (SecondCount >= 60) 
+		{
+			SecondCount = 0;
+			MinuteCount += 1;
+		}
+
+		if (MinuteCount <= 9) {
+			Minutebox.GetComponent<Text> ().text = "0" + MinuteCount + ":";
+		}
+		else
+		{
+			Minutebox.GetComponent<Text> ().text = "" + MinuteCount + ":";
+		}
+	}
+}
---
+++ Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+		public void NewLevel(string PlayType)
+		{
+			SceneManager.LoadScene (PlayType);
+		}
+		
+
+		public void Backbtn(string Back)
+		{
+			SceneManager.LoadScene (Back);
+		}
+
+		public void Creditsbtn(string Credits)
+		{
+			SceneManager.LoadScene (Credits);
+		}
+		
+		public void ExitGame()
+		{
+			Application.Quit ();
+		}
+	}
+
